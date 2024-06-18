8f0e3e03bc2d46de0e9052b012b4371a42ada108
hand <iswinter@aliyun.com>
2024-06-18 16:56:41 +0800

    实现战斗状态机
    实现战斗状态机，通过改变状态来实现战斗流程

--- src/scenes/BattleScene.ts
+++ src/scenes/BattleScene.ts
@@ -6,8 +6,19 @@ import { PlayerBattleMonster } from "../battle/monsters/player-battle-monster";
 import { HealthBar } from "../battle/ui/health-bar";
 import { BattleMenu } from "../battle/ui/menu/battle-menu";
 import { DIRECTION, DirectionType } from "../common/direction";
+import { StateMachine } from "../utils/state-machine";
 
-
+const BATTLE_STATES = Object.freeze({
+    INTRO:'INTRO',
+    PRE_BATTLE_INFO:'PRE_BATTLE_INFO',
+    BRING_OUT_MONSTER:'BRING_OUT_MONSTER',
+    PLAYER_INPUT:'PLAYER_INPUT',
+    ENEMY_INPUT:'ENEMY_INPUT',
+    BATTLE:'BATTLE',
+    POST_ATTACK_CHECK:'POST_ATTACK_CHECK',
+    FINISHED:'FINISHED',
+    FLEE_ATTEMPT:'FLEE_ATTEMPT'
+})
 export class BattleScene extends Scene {
     _battleMenu:BattleMenu;
     _cursorkeys: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
@@ -16,6 +27,7 @@ export class BattleScene extends Scene {
     _activeEnemyMonster:EnemyBattleMonster;
     _activePlayerMonster:PlayerBattleMonster;
     _activePlayerAttackIndex:number;
+    _battleStateMachine:StateMachine;
     constructor(){
         super('BattleScene')
         console.log('BattleScene load',this)
@@ -40,7 +52,7 @@ export class BattleScene extends Scene {
                     assetKey:MONSTER_ASSET_KEYS.CARNODUSK,
                     maxHp:25,
                     currentHp:25,
-                    baseAttack:25,
+                    baseAttack:5,
                     attackIds:[1],
                     currentLevel:5
                 },
@@ -58,7 +70,7 @@ export class BattleScene extends Scene {
                 assetKey:MONSTER_ASSET_KEYS.IGUANIGNITE,
                 maxHp:25,
                 currentHp:25,
-                baseAttack:5,
+                baseAttack:15,
                 attackIds:[2,1],
                 currentLevel:5
             },
@@ -69,11 +81,14 @@ export class BattleScene extends Scene {
         
         //创建信息框
         this._battleMenu = new BattleMenu(this,this._activePlayerMonster)
-        this._battleMenu.showMainBattleMenu()
+        
+        this._createBattleStateMachine()
+        
         //创建键盘 上下左右,空格 shift等热键 事件
         this._cursorkeys = this.input.keyboard?.createCursorKeys();
     }
     update(time: number, delta: number): void {
+        this._battleStateMachine.update()
         //空格键是否按下
         const wasSpaceKeyPressed = Phaser.Input.Keyboard.JustDown(this._cursorkeys?.space)
         // console.log(wasSpaceKeyPressed)
@@ -87,7 +102,7 @@ export class BattleScene extends Scene {
             this._activePlayerAttackIndex = this._battleMenu.selectedAttack
             if( !this._activePlayerMonster.attacks[this._activePlayerAttackIndex] ) return
             this._battleMenu.hideMonsterAttackSubMenu()
-            this._handleBattleSequene()
+            this._battleStateMachine.setState(BATTLE_STATES.ENEMY_INPUT)
 
             return
         }
@@ -112,16 +127,7 @@ export class BattleScene extends Scene {
         }
     }
      
-    _handleBattleSequene(){
-        //如果 玩家、敌人同时选择攻击， 生成战斗序列
-        //显示攻击信息，短暂暂停
-        //展示战斗动画，短暂暂停
-        //展示受伤动画，短暂暂停
-        //血条动画，短暂暂停
-        //换成另一只怪兽，重新走这个流程
-
-        this._playerAttack()
-    }
+ 
     _playerAttack(){
         this._battleMenu.updateInfoPanelMessageAndWaitForInput([`${this._activePlayerMonster.name} used ${this._activePlayerMonster.attacks[this._activePlayerAttackIndex].name}`],()=>{
             //引入时间插件
@@ -134,13 +140,13 @@ export class BattleScene extends Scene {
     }
     _enemyAttck(){
         if(this._activeEnemyMonster.isFainted){
-            this._postBattleSequeneCheck()
+            this._battleStateMachine.setState(BATTLE_STATES.POST_ATTACK_CHECK)
             return
         }
         this._battleMenu.updateInfoPanelMessageAndWaitForInput([`${this._activeEnemyMonster.name} used ${this._activeEnemyMonster.attacks[0].name}`],()=>{
             this.time.delayedCall(500,()=>{
                 this._activePlayerMonster.takeDamage(this._activeEnemyMonster.baseAttack,()=>{
-                    this._postBattleSequeneCheck()
+                    this._battleStateMachine.setState(BATTLE_STATES.POST_ATTACK_CHECK)
                 })
             })
         })
@@ -154,20 +160,20 @@ export class BattleScene extends Scene {
          */
         if(this._activeEnemyMonster.isFainted){
             this._battleMenu.updateInfoPanelMessageAndWaitForInput([`${this._activeEnemyMonster.name} fainted`,'You have gained some exp!'],()=>{
-                //过渡到世界地图
-                this._transitionToNextScene()
+                //过渡下个状态
+                this._battleStateMachine.setState(BATTLE_STATES.FINISHED)
             })
             return
         }
 
         if(this._activePlayerMonster.isFainted){
             this._battleMenu.updateInfoPanelMessageAndWaitForInput([`${this._activePlayerMonster.name} fainted`,'You have no more monsters'],()=>{
-                this._transitionToNextScene()
+                this._battleStateMachine.setState(BATTLE_STATES.FINISHED)
             })
             return
         }
 
-        this._battleMenu.showMainBattleMenu()
+        this._battleStateMachine.setState(BATTLE_STATES.PLAYER_INPUT)
     }
 
     _transitionToNextScene(){
@@ -178,5 +184,101 @@ export class BattleScene extends Scene {
             //完成回调
             this.scene.start('world')
         })
+    }
+
+    _createBattleStateMachine(){
+        this._battleStateMachine = new StateMachine('battle',this)
+        this._battleStateMachine.addState({
+            name:BATTLE_STATES.INTRO,
+            onEnter:()=>{
+                //等待任意场景设置并且转换动画完成
+                this.time.delayedCall(500,()=>{
+                    this._battleStateMachine.setState(BATTLE_STATES.PRE_BATTLE_INFO)
+                })
+            }
+        })
+        this._battleStateMachine.addState({
+            name:BATTLE_STATES.PRE_BATTLE_INFO,
+            onEnter:()=>{
+                //等待敌方怪兽出现在场景中并通知玩家相关信息
+                this.time.delayedCall(500,()=>{
+                    this._battleMenu.updateInfoPanelMessageAndWaitForInput([`${this._activeEnemyMonster.name} appear!`],()=>{
+                        //等待文本动画完成 并跳转下一个状态
+                        this.time.delayedCall(500,()=>{
+                            this._battleStateMachine.setState(BATTLE_STATES.BRING_OUT_MONSTER)
+                        })
+                    })
+                })
+            }
+        })
+        this._battleStateMachine.addState({
+            name:BATTLE_STATES.BRING_OUT_MONSTER,
+            onEnter:()=>{
+                //等待玩家怪兽出现并且 通知玩家相关信息
+                this.time.delayedCall(500,()=>{
+                    this._battleMenu.updateInfoPanelMessageAndWaitForInput([`go ${this._activePlayerMonster.name}!`],()=>{
+                        //等待文本动画完成 并跳转下一个状态
+                        //使用this.time.delayedCall函数，this._battleStateMachine.setState会被放到 微任务 中，
+                        //会先把isChangingState赋值为false，然后运行this._battleStateMachine.setState，BATTLE_STATES.PLAYER_INPUT不会被push到changingStateQueQue序列
+                        this.time.delayedCall(500,()=>{
+                            this._battleStateMachine.setState(BATTLE_STATES.PLAYER_INPUT)
+                        })
+                    })
+                })
+            }
+        })
+        this._battleStateMachine.addState({
+            name:BATTLE_STATES.PLAYER_INPUT,
+            onEnter:()=>{
+                this._battleMenu.showMainBattleMenu()
+            }
+        })
+        this._battleStateMachine.addState({
+            name:BATTLE_STATES.ENEMY_INPUT,
+            onEnter:()=>{
+                //为敌方随机选择一个招式，并在未来实现AI行为
+
+                //不使用this.time.delayedCall函数，this._battleStateMachine.setState不会放到 微任务 中，
+                //先运行this._battleStateMachine.setState，此时isChangingState会是true，
+                //BATTLE_STATES.BATTLE会被push到changingStateQueQue序列中，
+                //然后isChangingState才赋值为false
+                this._battleStateMachine.setState(BATTLE_STATES.BATTLE)
+            }
+        })
+        this._battleStateMachine.addState({
+            name:BATTLE_STATES.BATTLE,
+            onEnter:()=>{
+                //如果 玩家、敌人同时选择攻击， 生成战斗序列
+                //显示攻击信息，短暂暂停
+                //展示战斗动画，短暂暂停
+                //展示受伤动画，短暂暂停
+                //血条动画，短暂暂停
+                //换成另一只怪兽，重新走这个流程
+
+                this._playerAttack()
+            }
+        })
+        this._battleStateMachine.addState({
+            name:BATTLE_STATES.POST_ATTACK_CHECK,
+            onEnter:()=>{
+                this._postBattleSequeneCheck()
+            }
+        })
+        this._battleStateMachine.addState({
+            name:BATTLE_STATES.FINISHED,
+            onEnter:()=>{
+                this._transitionToNextScene()
+            }
+        })
+        this._battleStateMachine.addState({
+            name:BATTLE_STATES.FLEE_ATTEMPT,
+            onEnter:()=>{
+                this._battleMenu.updateInfoPanelMessageAndWaitForInput([`You got away safely!`],()=>{
+                    this._battleStateMachine.setState(BATTLE_STATES.FINISHED)
+                })
+            }
+        })
+        //启动状态机
+        this._battleStateMachine.setState(BATTLE_STATES.INTRO)
     }
 }
---
+++ src/utils/state-machine.ts
@@ -0,0 +1,103 @@
+type State = {
+    name:string,
+    onEnter?:()=>void|undefined,
+
+}
+export class StateMachine {
+    _state:Map<string,State>
+    _currentState:State|undefined;
+    _isChangingState:boolean;
+    /**
+     * 改变状态的序列，使状态按照顺序执行
+     */
+    _changingStateQueque:string[]
+
+    _id:string;
+    _context:object | undefined;
+    /**
+     * 
+     * @param id 
+     * @param context 
+     */
+    constructor(id:string,context?:object){
+        this._id = id
+        this._context = context
+        this._isChangingState = false
+        this._changingStateQueque = []
+        this._currentState = undefined
+        this._state = new Map()
+    }
+    get currentStateName(){
+        return this._currentState?.name
+    }
+    update(){
+        // console.log(this._changingStateQueque)
+        if(this._changingStateQueque.length > 0) {
+            this.setState(this._changingStateQueque.shift())
+            return
+        }
+    }
+    /**
+     * 
+     * @param name 
+     */
+    setState(name:string){
+        const methodName = 'setState'
+        //状态是否存在
+        if(!this._state.has(name)){
+            console.warn(`[${StateMachine.name}-${this._id}:${methodName}] try to change unKnown state ${name}`)
+            return
+        }
+
+        //name判断是不是当前状态
+        if(this._isCurrentState(name)){
+            return
+        }
+
+        if(this._isChangingState){
+            console.log('push:',name)
+            this._changingStateQueque.push(name)
+            return
+        }
+        this._isChangingState = true
+        console.log('正在改变状态1：',this._isChangingState)
+        console.log(`[${StateMachine.name}-${this._id}:${methodName}] change from ${this._currentState?.name ?? 'none'} to ${name}`)
+
+        this._currentState = this._state.get(name)
+
+        if(this._currentState?.onEnter){
+            console.log(`[${StateMachine.name}-${this._id}:${methodName}] ${this._currentState.name} 被调用`)
+            this._currentState.onEnter()
+        }
+        this._isChangingState = false
+        console.log('正在改变状态2：',this._isChangingState)
+
+    }
+    /**
+     * 
+     * @param state 
+     */
+    addState(state:State){
+        this._state.set(state.name,{
+            name:state.name,
+            //如果 _context 存在，将 state.onEnter 方法绑定到 _context，否则保持原样，
+            //bind 方法确保方法调用时的 this 值是指定的对象。
+            //bind 确保 state.onEnter 方法在任何调用情况下都能正确访问 _context 对象
+            //使用 bind 方法可以避免 this 关键字的值在不同调用上下文中不一致的问题。
+            onEnter:this._context ? state.onEnter?.bind(this._context) : state.onEnter
+        })
+    }
+    /**
+     * 
+     * @param name 
+     * @returns 
+     */
+    _isCurrentState(name:string){
+        //有没有当前状态
+        if(!this._currentState){
+            return false
+        }
+
+        return this._currentState.name === name
+    }
+}
